3949269aafa5812149fdc484dc2811aafe5ee8a4
safarow <asifseferov00@gmail.com>
2023-05-21 11:49:47 +0400

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+  const [solution, setSolutions] = useState(null)
+
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions').then(res => res.json()).then(json => {
+      const randomSolution = json[Math.floor(Math.random() * json.length)]
+      setSolutions(randomSolution.word)
+    })
+  }, [setSolutions])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{fontSize: '24px'}}>Wordle AZ</h1>
+      { solution &&  <div style={{color: '#fff'}}> Solution is: {solution}</div>}
     </div>
   );
 }
